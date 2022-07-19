0596108dec756f661091a8dbd8d812deb105c776
unknown <darkskiiittles@gmail.com>
2022-07-19 06:58:58 -0400

    temporary fix for darkmeowfi and darkmeowshop IMGs

--- pages/works.js
+++ pages/works.js
@@ -10,8 +10,8 @@ import thumbProject from '../public/images/works/bg (1).png'
 import thumbBandingo from '../public/images/works/bandingo.png'
 import thumbAidanPaul from '../public/images/works/apmusic.png'
 import thumbCollab from '../public/images/works/devShop.png'
-import thumbDarkMeowFi from '../public/images/works/darkMeowFi.png'
-import thumbDarkMeowShop from '../public/images/works/darkMeowShop.png'
+// import thumbDarkMeowFi from '../public/images/works/darkMeowFi.png'
+// import thumbDarkMeowShop from '../public/images/works/darkMeowShop.png'
 import thumbEcommerce from '../public/images/works/tanjiroScreenshot.png'
 import thumbThreeJS from '../public/images/works/ThreeJSPortfolio.png'
 import thumbMatiasSanes from '../public/images/works/4.png'
@@ -51,11 +51,11 @@ const Works = () => {
                         </WorkGridItem>
                     </Section>
                     <Section delay={0.6}>
-                        <WorkGridItem id="darkMeowFi" title="DarkMeow Fi" thumbnail={thumbDarkMeowFi}>
+                        <WorkGridItem id="darkMeowFi" title="DarkMeow Fi" thumbnail="">
                         </WorkGridItem>
                     </Section>
                     <Section delay={0.7}>
-                        <WorkGridItem id="darkMeowShop" title="DarkMeow Shop" thumbnail={thumbDarkMeowShop}>
+                        <WorkGridItem id="darkMeowShop" title="DarkMeow Shop" thumbnail="">
                         </WorkGridItem>
                     </Section>
                     <Section delay={0.8}>
